cce3514472f783c7a131cf6e8a598bc5dbe20f52
james r. eng <jimeng@chai.umdl.umich.edu>
2012-10-18 10:45:22 -0400

    Small changes to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,7 +20,7 @@ ActiveRecord::Schema.define(:version => 20121016170838) do
     t.string   "user_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
-    t.string   "preferredStyle"
+    t.string   "preferred_style"
     t.string   "preferred_locale"
   end
 
