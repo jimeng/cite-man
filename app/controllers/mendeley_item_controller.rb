a53cd4a75bfbb5bb3595dd0e11f0ad10cce99687
Jim Eng <jimeng@umich.edu>
2012-10-18 20:07:11 -0400

    Worked on oauth for mendeley.

--- app/controllers/mendeley_item_controller.rb
+++ app/controllers/mendeley_item_controller.rb
@@ -13,29 +13,54 @@ class MendeleyItemController < ActionController::Base
     #Rails.backtrace_cleaner.remove_silencers!
     logger.info("MendeleyItemController.index")
 
-    collection_path = "oapi/library?consumer_key=#{@source.code01}"
+    if session[:access_token]
+      render_citations
+    elsif session[:request_token]
+      @request_token = session[:request_token]
+      @access_token = MendeleyItem.getAccessToken(@request_token)
+      session[:access_token] = @access_token
+      cookies[:mendeley_access_token] = 'true'
+      render_citations
+    else
+      @request_token = MendeleyItem.getRequestToken(source_mendeley_item_index_path(@source)) 
+      session[:request_token] = @request_token
+      r_token = {}
+      r_token[:request_token] = @request_token
 
-    begin
+      r_token[:authorize_url] = @request_token.authorize_url
+
+      Rails.logger.info(r_token)
+      respond_to do |format|
+        format.json { render :json => r_token }
+      end
+    end
+  end
+
+  def render_citations
+      collection_path = "oapi/library?consumer_key=#{@source.code01}"
+
+      begin
+        # debugger
+        @items = MendeleyItem.find( :all, :from => collection_path ) || []
+      rescue => e
+        Rails.logger.info(e.message)
+      end
+
+      #Rails.logger.info(@items)
       # debugger
-      @items = MendeleyItem.find( :all, :from => collection_path ) || []
-    rescue => e
-      Rails.logger.info(e.message)
-    end
 
-    #Rails.logger.info(@items)
-    # debugger
+       #Rails.logger.info('===============================')
 
-     #Rails.logger.info('===============================')
+       @styled = CslProcessor.formatCitations('mla-notes', @items)
 
-     @styled = CslProcessor.formatCitations('mla-url', @items)
+      #logger.info(@styled)
+      #Rails.logger.info('===============================')
 
-    #logger.info(@styled)
-    #Rails.logger.info('===============================')
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render :json => @items }
+      end
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @items }
-    end
   end
 
   def authenticate
--- app/models/mendeley_item.rb
+++ app/models/mendeley_item.rb
@@ -4,4 +4,27 @@ class MendeleyItem < ActiveResource::Base
   self.site = "http://api.mendeley.com"
   #self.element_name = "items"
 
+  def MendeleyItem.getRequestToken(callbackUrl)
+    consumer = OAuth::Consumer.new( "97f4e0896ab36d7b55e5f1a2b81e396c05010616b","034392514f666e3f236973d935757097", {
+      :site				  => self.site,
+      :scheme             => :header,
+      :http_method        => :get,
+      :request_token_path => "/oauth/request_token/",
+      :access_token_path  => "/oauth/access_token/",
+      :authorize_path     => "/oauth/authorize/",
+      :callback 		  => callbackUrl
+    })
+    request_token = consumer.get_request_token
+    Rails.logger.info(request_token)
+    authorize_url = request_token.authorize_url
+    Rails.logger.info(authorize_url)
+    return request_token
+  end
+
+  def MendeleyItem.getAccessToken(request_token)
+  	access_token = request_token.get_access_token()
+  	return access_token
+  end
+
+
 end
